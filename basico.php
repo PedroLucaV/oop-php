27db0a06ff9729aed074d07f8e469a0c7ebb3355
Pedro Lucas <izielucas2016@gmail.com>
2025-04-25 09:23:45 -0300

    basic structure of oop php initialized with a class

---
+++ basico.php
@@ -0,0 +1,10 @@
+<?php 
+    //Criação de uma classe:
+    class Person{
+        
+    }
+
+    //Para criar uma instancia dessa classe, usamos o new:
+    $instancia = new Person();
+
+    echo var_dump($instancia);
